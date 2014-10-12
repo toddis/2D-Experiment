dc0211395f311d7a2846c1ae68fe92791f201c8e
toddis <toddis@uw.edu>
2014-10-12 12:06:42 -0700

    Initialized project.

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	public float speed;
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		
+		Vector2 movement = new Vector2 (moveHorizontal, 0);
+		rigidbody2D.AddForce (movement * speed, ForceMode2D.Impulse);
+
+	}
+}
